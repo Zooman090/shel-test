aa797d078a9939d9ccc77ff8cd01be172fba4b63
Dmytro Paniontko <zooman090@gmail.com>
2018-03-04 11:19:31 +0200

    Setup aws-nodejs template

---
+++ src/handler.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const { getNearPlace } = require('request');
+
+module.exports.map = (event, context, callback) => {
+
+  callback(null);
+};
---
+++ src/map-api.js
@@ -0,0 +1,22 @@
+const request = require("request");
+
+exports.getNearPlace = (event) => {
+  request({
+    url: "https://api.foursquare.com/v2/venues/explore",
+    method: "GET",
+    qs: {
+      client_id: "CLIENT_ID",
+      client_secret: "CLIENT_SECRET",
+      ll: "40.7243,-74.0018",
+      query: "coffee",
+      v: "20170801",
+      limit: 1
+    }
+  }, function(err, res, body) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(body);
+    }
+  });
+};
